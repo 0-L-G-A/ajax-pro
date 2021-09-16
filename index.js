1d30fbd9fc160f62c05cfd24675f1ad963ae5c9a
0-L-G-A <lol79lol97@gmail.com>
2021-09-16 23:21:16 +0300

    layout

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 const buttonLoadInfo = document.querySelector('.btnLoadInfo');
-
+const wrapperForUsers = document.querySelector('.wrap');
 buttonLoadInfo.addEventListener("click", getData);
 
 
 async function getData(){
     let res = await fetch("http://localhost:2020/users", )
-    
     const json = await streamToJSON(res)
-    console.log(json)
-
+    json.users.map(user => {return wrapperForUsers.insertAdjacentHTML('beforeend',
+        `<h2>User ID: ${user.id}</h2>
+        <p>Name: ${user.first_name}</p>
+        <p>Lastname: ${user.last_name}</p>
+        <p>Email: ${user.email}</p>
+        <img src="${user.photo}" alt="userAvatar" class="avatar">`
+    );
+    })
+    buttonLoadInfo.hidden = true;
 }
 
 async function streamToJSON(response){
